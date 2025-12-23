31758e55c9253b7a34963b3d1897664738b2db50
Benyamin Shoraki <benyaminchor@gmail.com>
2025-12-23 12:35:24 +0200

    Fix SQL syntax in init.sql and update MySQLConnector to handle optional SQL file initialization

--- app/db/init.sql
+++ app/db/init.sql
@@ -7,6 +7,6 @@ CREATE TABLE IF NOT EXISTS contact (
     id int,
     first_name varchar(50) DEFAULT NULL,
     last_name varchar(50) DEFAULT NULL,
-    phone_number: varchar(50),
+    phone_number varchar(50),
     PRIMARY KEY (id)
 );
